c09ce9a50a3ffff5b16eedc266f415fb72d5151f
amanda <amanda.peres@iesb.edu.br>
2023-04-10 20:58:43 -0300

    api

---
+++ pages/deputados/hooks.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
+import Navbar from '../../components/Navbar';
+
+const hooks = () => {
+  return (
+    <Navbar titulo="Deputados">
+        
+       <h1>{nome}</h1>
+
+
+    </Navbar>
+  )
+}
+
+export default hooks
---
+++ pages/deputados/index.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
+import Navbar from '../../components/Navbar'
+
+const index = () => {
+  return (
+   
+   <Navbar titulo="Deputados">
+    
+   </Navbar>
+   
+  )
+}
+
+export default index
